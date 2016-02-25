1d5576f34f6eab332db0049283ef98a6059e3e9e
hansenji <jordanihansen10@gmail.com>
2016-02-25 11:25:59 -0700

    Release 0.1.0

--- library/src/test/java/com/vikingsen/pocketbus/BusTest.java
+++ library/src/test/java/com/vikingsen/pocketbus/BusTest.java
@@ -1,37 +1,12 @@
 package com.vikingsen.pocketbus;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class BusTest {
 
-    private Bus bus;
-
-    @Before
-    public void setup() {
-        bus = Bus.getDefault();
-    }
-
     @Test
     public void testPost() {
-        bus.register(new Subscription<Foo>() {
-            @Override
-            public void handle(Foo foo) {
-                assertEquals("Foo", foo.getClass().getSimpleName());
-            }
 
-            @Override
-            public Class<Foo> getEventClass() {
-                return Foo.class;
-            }
-
-            @Override
-            public ThreadMode getThreadMode() {
-                return ThreadMode.CURRENT;
-            }
-        });
     }
 
 }
